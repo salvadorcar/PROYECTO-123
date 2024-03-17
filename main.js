97e8f483c2cc1624c673406a199d15c3a570e02c
salvadorcar <143309978+salvadorcar@users.noreply.github.com>
2024-03-17 14:33:37 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+preload()
+
+setup(canvas)
+
+draw()
+
+function preload() {   
+}
+
+function setup()  {
+  canvas = cratecanvas(300, 300)
+  canvas.center();
+}
+
+function draw() {
+}
+
+function take_snapshopt() {
+    SVGAElement('myFilterImage.png');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
